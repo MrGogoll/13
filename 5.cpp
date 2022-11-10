de8ad2573520cda1637c0f831bb306abe28adad5
MrGogoll <115408973+MrGogoll@users.noreply.github.com>
2022-11-10 19:53:51 +0300

    13

---
+++ 5.cpp
@@ -0,0 +1,14 @@
+﻿// №1 есть два числа - A и B. Задать им значения самостоятельно. Вывести их сумму (то есть A+B).
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	int a, b;
+	cin >> a, b;
+	int summ;
+	summ = a + b;
+	cout << summ;
+}
+
